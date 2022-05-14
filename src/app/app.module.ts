10b0d405f1c0e1624331abce3d7dfdabc4d13e31
isanCrish <isancrish1996@gmail.com>
2022-05-14 15:50:24 +0530

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RankingsComponent } from './rankings/rankings.component';
+import { RankingComponent } from './rankings/ranking/ranking.component';
+import { RankingListComponent } from './rankings/ranking-list/ranking-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RankingsComponent,
+    RankingComponent,
+    RankingListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
